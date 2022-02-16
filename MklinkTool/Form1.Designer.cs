653ff00538378eb14e5acac26a8414dff3c81721
Lss233 <8984680+lss233@users.noreply.github.com>
2022-02-16 17:53:25 +0800

    Add project files.

---
+++ MklinkTool/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MklinkTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonMklink = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.checkBoxAddPrefix = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(940, 598);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listbox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectAll.Location = new System.Drawing.Point(993, 133);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(207, 56);
+            this.buttonSelectAll.TabIndex = 1;
+            this.buttonSelectAll.Text = "Select All";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.buttonSelectAll_Click);
+            // 
+            // buttonMklink
+            // 
+            this.buttonMklink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMklink.Location = new System.Drawing.Point(993, 35);
+            this.buttonMklink.Name = "buttonMklink";
+            this.buttonMklink.Size = new System.Drawing.Size(207, 56);
+            this.buttonMklink.TabIndex = 2;
+            this.buttonMklink.Text = "Mklink";
+            this.buttonMklink.UseVisualStyleBackColor = true;
+            this.buttonMklink.Click += new System.EventHandler(this.buttonMklink_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClear.Location = new System.Drawing.Point(993, 231);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(207, 56);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // checkBoxAddPrefix
+            // 
+            this.checkBoxAddPrefix.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxAddPrefix.AutoSize = true;
+            this.checkBoxAddPrefix.Checked = true;
+            this.checkBoxAddPrefix.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAddPrefix.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxAddPrefix.Location = new System.Drawing.Point(1002, 337);
+            this.checkBoxAddPrefix.Name = "checkBoxAddPrefix";
+            this.checkBoxAddPrefix.Size = new System.Drawing.Size(124, 22);
+            this.checkBoxAddPrefix.TabIndex = 4;
+            this.checkBoxAddPrefix.Text = "Add prefix";
+            this.checkBoxAddPrefix.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(990, 386);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "<- Drag files to here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1212, 624);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxAddPrefix);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonMklink);
+            this.Controls.Add(this.buttonSelectAll);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mklink Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private Button buttonSelectAll;
+        private Button buttonMklink;
+        private Button buttonClear;
+        private CheckBox checkBoxAddPrefix;
+        private Label label1;
+    }
+}
+
---
+++ MklinkTool/Form1.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MklinkTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //listBox1.SelectedItem;
+            //DragDrop.DoDragDrop(this, new DataObject(DataFormats.FileDrop, paths), DragDropEffects.Link);
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(sender is Button)
+            {
+                return;
+            }
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+        }
+
+        private void listbox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string path in paths)
+            {
+                if (Directory.Exists(path))
+                {
+                    string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                    foreach(var file in files)
+                    {
+                        if (!listBox1.Items.Contains(file))
+                            listBox.Items.Add(file);
+                    }
+                } else
+                {
+                    if (!listBox1.Items.Contains(path))
+                        listBox.Items.Add(path);
+                }
+                
+            }
+                
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+        }
+
+        private void buttonSelectAll_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < this.listBox1.Items.Count; i++)
+            {
+                this.listBox1.SetSelected(i, true);
+            }
+        }
+
+        public async Task<int> GetTargetHandle()
+        {
+            int currrentHandle = GetForegroundWindow().ToInt32();
+            Console.WriteLine("Starting parser");
+            var taskResult = await Task.Run(() =>
+            {
+                int handle = 0;
+                while(true)
+                {
+                    handle = GetForegroundWindow().ToInt32();
+                    if (handle != currrentHandle && handle > 0)
+                    {
+                        return handle;
+                    }
+                    Thread.Sleep(100);
+                }
+            });
+            return taskResult;
+        }
+        
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [STAThread]
+        public static string GetWindowPath(int handle, dynamic windows)
+        {
+            //Default value for the variable returned at the end of the method if no path was found
+            String Path = "";
+
+            //SHDocVw.ShellWindows shellWindows = new SHDocVw.ShellWindows();
+            foreach (SHDocVw.InternetExplorer window in windows)
+            {
+                Console.WriteLine(window.LocationURL);
+                //If Window handle corresponds to passed handle
+                if (window.HWND == handle)
+                {
+                    //Print path of the respective folder and save it within the returned variable
+                    Console.WriteLine(window.LocationURL);
+                    Path = window.LocationURL;
+                    break;
+                }
+            }
+            //If a path was found, retun the path, otherwise return ""
+            return Path;
+        }
+
+        private void buttonMklink_Click(object sender, EventArgs e)
+        {
+            if(this.listBox1.SelectedItems.Count < 1)
+            {
+                MessageBox.Show("You need to select files first!", "You fucking NERDS", MessageBoxButtons.OK);
+                return;
+            }
+            using (var fbd = new CommonOpenFileDialog { IsFolderPicker = true })
+            {
+                CommonFileDialogResult result = fbd.ShowDialog();
+
+                if (result == CommonFileDialogResult.Ok)
+                {
+                    string[] paths = new string[this.listBox1.SelectedItems.Count];
+                    for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                    {
+                        string file = (string)this.listBox1.SelectedItems[i];
+                        paths[i] = "\\\"" + file + "\\\"";
+                        paths[i] = "\"" + file + "\"";
+                    }
+                    
+                    foreach (string path in paths)
+                    {
+                        string[] slash = path.Split("\\".ToCharArray());
+                        string pathFileName = slash[slash.Length - 1];
+                        if (checkBoxAddPrefix.Checked)
+                        {
+                            pathFileName = "linked-" + pathFileName;
+                        }
+                        
+                        string target = "\"" + fbd.FileName +"\\" + pathFileName;
+                        Process process = new Process();
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        startInfo.WorkingDirectory = @"C:\Windows\System32";
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = "/user:Administrator \"cmd /C mklink " +  target + " " + path + " >> logs.txt\"";
+                        Console.WriteLine(startInfo.Arguments);
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    }
+                }
+            }
+            //var shell = new Shell32.Shell();
+            //shell.MinimizeAll();
+            //var windows = shell.Windows();
+
+            //foreach (SHDocVw.InternetExplorer window in windows) {
+            //    Console.WriteLine("Current handle = " + window.FullName);
+            //}
+            //    GetTargetHandle().ContinueWith(handle => {
+            //    Console.WriteLine("Current handle = " + handle.Result);
+            //    GetWindowPath(handle.Result, windows);
+            //});
+
+            //buttonMklink.DoDragDrop(new DataObject(DataFormats.FileDrop, paths),
+            //    DragDropEffects.Link);
+        }
+    }
+}
